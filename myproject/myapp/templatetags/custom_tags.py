4e2069b7840a3069e187dc3f8df8147d13cc7be4
MaxKompa <max.kompa645@gmail.com>
2025-03-16 18:44:56 +0100

    Template tags

---
+++ myproject/myapp/templatetags/custom_tags.py
@@ -0,0 +1,19 @@
+﻿from django import template
+from datetime import datetime
+
+register = template.Library()
+
+@register.simple_tag
+def current_date():
+    return datetime.now().strftime('%Y-%m-%d')
+
+
+@register.simple_tag
+def truncate_text(text, length=50):
+    if len(text) > length:
+        return text[:length] + "..."
+    return text
+
+@register.inclusion_tag('user_list.html')
+def show_users(users):
+    return {'users': users}
